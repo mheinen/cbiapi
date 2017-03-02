7931def951e421500983b9900e13f3e9c7cf5791
mheinen <heinen.m@web.de>
2017-03-02 11:27:28 +0100

    added select intent logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,28 @@
 class ApplicationController < ActionController::API
+
   def base
+
     puts params.inspect
-    render json: {nameEins: params[:name1], nameZwei: params[:name2]}
+    case params[:intent]
+      when 'Greeting'
+        render json: {nameEins: params[:name1], nameZwei: params[:name2]}
+      when 'Select'
+        render getAll(params[:tablename])
+    end
+
   end
+
+  def getAll(tablename)
+
+    case tablename
+      when 'Kunden'
+        render json: {counter: 100}
+      when 'Artikel'
+        render json: {counter: 900}
+      else
+        render json: {counter: 0}
+    end
+
+  end
+
 end
